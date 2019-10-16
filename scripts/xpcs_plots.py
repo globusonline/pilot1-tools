6419079c78201bbe62b68f2c80f036e39a86166b
Rick Wagner <rick@globus.org>
2019-10-16 14:14:31 -0700

    plots for fits

--- scripts/xpcs_plots.py
+++ scripts/xpcs_plots.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import numpy
 import matplotlib
@@ -43,7 +44,7 @@ def gen_image(dset, basename, cbar=True, log=False):
     plt.savefig(image_filename, dpi=100)
 
 def plot_pixelSum(xpcs_h5file):
-    h5fname = xpcs_h5file.filename
+    h5fname = os.path.basename(xpcs_h5file.filename)
     pixelSum_dset = xpcs_h5file['exchange/pixelSum']
     basename = h5fname.rstrip('.hdf') + '_pixelSum'
     gen_image(pixelSum_dset, basename)
@@ -52,7 +53,7 @@ def plot_pixelSum(xpcs_h5file):
     gen_image(pixelSum_dset, basename + '_pre', cbar=False, log=True)
 
 def plot_intensity_vs_time(xpcs_h5file):
-    basename = xpcs_h5file.filename.rstrip('.hdf')
+    basename = os.path.basename(xpcs_h5file.filename).rstrip('.hdf')
     i_vs_t = xpcs_h5file['exchange/frameSum']
     pylab.plot(i_vs_t[0], i_vs_t[1])
     plt.xlabel("Elapsed Time (s)")
@@ -61,7 +62,7 @@ def plot_intensity_vs_time(xpcs_h5file):
     plt.savefig(basename + '_frameSum.png')
 
 def plot_intensity_t_vs_q(xpcs_h5file):
-    basename = xpcs_h5file.filename.rstrip('.hdf')
+    basename = os.path.basename(xpcs_h5file.filename).rstrip('.hdf')
     q = xpcs_h5file['/xpcs/sqlist']
     pmt_t = xpcs_h5file['/exchange/partition-mean-partial']
     markers = ['o', 'x', '+', 'v', '^', '<', '>', 's', 'p', '*', 'h',
@@ -96,7 +97,7 @@ def plot_intensity_t_vs_q(xpcs_h5file):
     plt.savefig('{}_intensity_t.png'.format(basename), dpi=150)
 
 def plot_intensity_vs_q(xpcs_h5file):
-    basename = xpcs_h5file.filename.rstrip('.hdf')
+    basename = os.path.basename(xpcs_h5file.filename).rstrip('.hdf')
     q = xpcs_h5file['/xpcs/sqlist']
     pmt = xpcs_h5file['/exchange/partition-mean-total']
     fig = plt.figure()
@@ -113,7 +114,7 @@ def plot_g2_all(xpcs_h5file):
         fig.suptitle('{} Correlations g2 {:d} to {:d}'.format(bn, gs, ge))
         plt.savefig('{}_g2_corr_{:03d}_{:03d}.png'.format(bn, gs, ge), dpi=100)
     
-    basename = xpcs_h5file.filename.rstrip('.hdf')
+    basename = os.path.basename(xpcs_h5file.filename).rstrip('.hdf')
     exp = xpcs_h5file['/measurement/instrument/detector/exposure_period']
     dt = xpcs_h5file['/exchange/tau'][0]*exp[0][0]
     g2_all = xpcs_h5file['/exchange/norm-0-g2']
@@ -141,17 +142,141 @@ def plot_g2_all(xpcs_h5file):
                     break
         sfig(basename, g_start, g_index - 1)
 
+def plot_g2_all_fit(xpcs_h5file):
+    def sfig(bn, gs, ge):
+        fig.suptitle('{} Correlation Fitting Result'.format(bn))
+        plt.savefig('{}_g2_corr_fit{:03d}_{:03d}.png'.format(bn, gs, ge), dpi=100)
+    
+    basename = os.path.basename(xpcs_h5file.filename).rstrip('.hdf')
+    exp = xpcs_h5file['/measurement/instrument/detector/exposure_period']
+    dt = xpcs_h5file['/exchange/tau'][0]*exp[0][0]
+    g2_all = xpcs_h5file['/exchange/norm-0-g2']
+    g2_err_all = xpcs_h5file['/exchange/norm-0-stderr']
+    g2_fit1_all = xpcs_h5file['/exchange/g2avgFIT1'][:,0,:]
+    g2_fit2_all = xpcs_h5file['/exchange/g2avgFIT2'][:,0,:]
+    dynamicQ = xpcs_h5file['/xpcs/dqlist'][0]
+    n_plots = dynamicQ.shape[0]
+
+    g_index = 0
+    while g_index < n_plots:
+        g_start = g_index
+        pylab.clf()
+        fig, axs = plt.subplots(nrows=3, ncols=3, constrained_layout=True)
+        fig.set_size_inches((10,10.25))
+        for i in range(0, 3): # x, left-right axis
+            for j in range(0, 3): # y, top-down axis
+                ax = axs[i,j]
+                ax.plot(dt, g2_fit1_all[:, g_index], 'b')
+                ax.plot(dt, g2_fit2_all[:, g_index], 'r')
+                ax.errorbar(dt, g2_all[:, g_index], yerr=g2_err_all[:, g_index],
+                                fmt='ko', fillstyle='none', capsize=2, markersize=5)
+                ax.set_title('q={:f}'.format(dynamicQ[g_index]))
+                ax.set_yscale('linear')
+                ax.set_xscale('log')
+                g_index += 1
+                if g_index == n_plots:
+                    sfig(basename, g_start, g_index - 1)
+                    break
+        sfig(basename, g_start, g_index - 1)
+
+def plot_fits(xpcs_h5file):
+    
+    basename = os.path.basename(xpcs_h5file.filename).rstrip('.hdf')
+
+    x = xpcs_h5file['/xpcs/dqlist'][0]
+
+    c1 = xpcs_h5file['exchange/contrastFIT1'][0]
+    c2 = xpcs_h5file['exchange/contrastFIT2'][0]
+    c1_err = xpcs_h5file['exchange/contrastErrFIT1'][0]
+    c2_err = xpcs_h5file['exchange/contrastErrFIT2'][0]
+   
+    b1 = xpcs_h5file['exchange/baselineFIT1'][0]
+    b2 = xpcs_h5file['exchange/baselineFIT2'][0]
+    b1_err = xpcs_h5file['exchange/baselineErrFIT1'][0]
+    b2_err = xpcs_h5file['exchange/baselineErrFIT2'][0]
+
+    tau1 = xpcs_h5file['exchange/tauFIT1'][0]
+    tau2 = xpcs_h5file['exchange/tauFIT2'][0]
+    tau1_err = xpcs_h5file['exchange/tauErrFIT1'][0]
+    tau2_err = xpcs_h5file['exchange/tauErrFIT2'][0]
+
+    exp2 = xpcs_h5file['exchange/exponentFIT2'][0]
+    exp2_err = xpcs_h5file['exchange/exponentErrFIT2'][0]
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
+    fig.set_size_inches((10,10.25))
+    
+    ax = axs[0,0]
+    ax.errorbar(x, c1, yerr=c1_err,
+                    fmt='bo', fillstyle='none',
+                    capsize=2, markersize=5, label='Simple Exp')
+    ax.errorbar(x, c2, yerr=c2_err,
+                    fmt='ro', fillstyle='none',
+                    capsize=2, markersize=5, label='Stretched Exp')
+    ax.set_xlabel("q (A^-1)")
+    ax.set_ylabel("Contrast")
+    ax.set_yscale('linear')
+    ax.set_xscale('log')    
+    ax.set_ylim([-0.5, 1])
+    ax.legend(numpoints=1)
+    
+    ax = axs[0,1]
+    ax.errorbar(x, b1, yerr=b1_err,
+                    fmt='bo', fillstyle='none',
+                    capsize=2, markersize=5, label='Simple Exp')
+    ax.errorbar(x, b2, yerr=b2_err,
+                    fmt='ro', fillstyle='none',
+                    capsize=2, markersize=5, label='Stretched Exp')
+    ax.set_xlabel("q (A^-1)")
+    ax.set_ylabel("Baseline")
+    ax.set_yscale('linear')
+    ax.set_xscale('log')    
+    ax.legend(numpoints=1)
+
+    ax = axs[1,1]
+    ax.errorbar(x, tau1, yerr=tau1_err,
+                    fmt='bo', fillstyle='none',
+                    capsize=2, markersize=5, label='Simple Exp')
+    ax.errorbar(x, tau2, yerr=tau2_err,
+                    fmt='ro', fillstyle='none',
+                    capsize=2, markersize=5, label='Stretched Exp')
+    ax.set_xlabel("q (A^-1)")
+    ax.set_ylabel("Tau (sec)")
+    ax.set_yscale('log')
+    ax.set_xscale('log')    
+    ax.legend(numpoints=1)
+
+    ax = axs[1,0]
+    ax.errorbar(x, exp2, yerr=exp2_err,
+                    fmt='ro', fillstyle='none',
+                    capsize=2, markersize=5, label='Stretched Exp')
+    ax.set_xlabel("q (A^-1)")
+    ax.set_ylabel("Stretching Exponent")
+    ax.set_yscale('linear')
+    ax.set_xscale('log')    
+    ax.set_ylim([0, 2.25])
+    ax.legend(numpoints=1)
+    
+    fig.suptitle('{} Correlation Fitting Parameters'.format(basename))
+    plt.savefig('{}_corr_params.png'.format(basename), dpi=100)
+
+
+        
 if __name__ == '__main__':
     import sys
     h5filename = sys.argv[1]
     print('opening ' + h5filename)
     x_h5_file = h5py.File(h5filename, 'r')
     plot_intensity_vs_time(x_h5_file)
-    pylab.clf()
+    plt.close('all')
     plot_intensity_vs_q(x_h5_file)
-    pylab.clf()
+    plt.close('all')
     plot_intensity_t_vs_q(x_h5_file)
-    pylab.clf()
+    plt.close('all')
     plot_g2_all(x_h5_file)
-    pylab.clf()
+    plt.close('all')
+    plot_g2_all_fit(x_h5_file)
+    plt.close('all')
     plot_pixelSum(x_h5_file)
+    plt.close('all')
+    plot_fits(x_h5_file)
